acf3c2a9d28e9c02368ddb6a110342bd21bdcee1
Hang <yhnbgfd@gmail.com>
2018-07-16 14:31:16 +0800

    人脸识别OK（CPU占用有点高）

---
+++ Hang.EmguCV.Demo.Net40/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Hang.EmguCV.Demo.Net40
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imgCamUser = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // imgCamUser
+            // 
+            this.imgCamUser.Location = new System.Drawing.Point(12, 12);
+            this.imgCamUser.Name = "imgCamUser";
+            this.imgCamUser.Size = new System.Drawing.Size(776, 426);
+            this.imgCamUser.TabIndex = 2;
+            this.imgCamUser.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imgCamUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Emgu.CV.UI.ImageBox imgCamUser;
+    }
+}
+
---
+++ Hang.EmguCV.Demo.Net40/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Hang.EmguCV.Demo.Net40
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private CascadeClassifier _cascadeClassifier;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture();
+            _cascadeClassifier = new CascadeClassifier(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "haarcascade_frontalface_default.xml"));
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            using (var imageFrame = _capture.QueryFrame().ToImage<Bgr, Byte>())
+            {
+                if (imageFrame != null)
+                {
+                    var grayframe = imageFrame.Convert<Gray, byte>();
+                    var faces = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, Size.Empty); //the actual face detection happens here
+
+                    foreach (var face in faces)
+                    {
+                        imageFrame.Draw(face, new Bgr(Color.BurlyWood), 3); //the detected face(s) is highlighted here using a box that is drawn around it/them
+                    }
+                }
+                imgCamUser.Image = imageFrame;
+            }
+        }
+
+    }
+}
